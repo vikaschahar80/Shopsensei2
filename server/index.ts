a542146d3e378cb5ce6d24305d6f14d5c3c295d3
Vikash Chahar <vikas@Vikashs-MacBook-Air.local>
2026-05-15 22:08:52 +0530

    fix: globally force IPv4 to resolve Supabase connection issues on Render

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,6 @@
 import 'dotenv/config';
+import dns from 'node:dns';
+dns.setDefaultResultOrder('ipv4first');
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
